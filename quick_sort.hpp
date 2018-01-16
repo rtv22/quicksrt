1d9e1d36b0521320b0fb0597529584dc76c8d8db
Igor <rtvcojigor@gmail.com>
2018-01-17 00:46:28 +0300

    first commit

---
+++ quick_sort.hpp
@@ -0,0 +1,131 @@
+#include <future>
+#include <sort.hpp>
+#include <thread_safe_stack.hpp>
+#include <thread>
+#include <vector>
+
+
+template <typename BidirectionalIterator>
+class parallel_quick_sorter_t {
+
+private:
+    struct chunk_to_sort_t {
+
+        BidirectionalIterator first;
+        BidirectionalIterator last;
+        std::promise<void> promise;
+    };
+
+public:
+    parallel_quick_sorter_t();
+    parallel_quick_sorter_t(parallel_quick_sorter_t const & other) = delete;
+    auto operator =(parallel_quick_sorter_t const & other)->parallel_quick_sorter_t & = delete;
+    parallel_quick_sorter_t(parallel_quick_sorter_t && other) = delete;
+    auto operator =(parallel_quick_sorter_t && other)->parallel_quick_sorter_t & = delete;
+    ~parallel_quick_sorter_t();
+
+    void do_sort(BidirectionalIterator first, BidirectionalIterator last);
+    void sort_thread();
+    void try_sort_chunk();
+    void sort_chunk(std::shared_ptr<chunk_to_sort_t> chunk);
+
+private:
+    std::vector<std::thread> threads_;
+    unsigned int const max_threads_count_;
+    stack<std::shared_ptr<chunk_to_sort_t>> chunks_;
+    std::atomic<bool> end_of_data_;
+    mutable std::mutex mutex_;
+};
+
+
+
+template <typename BidirectionalIterator>
+parallel_quick_sorter_t<BidirectionalIterator>::
+parallel_quick_sorter_t()
+    :
+    max_threads_count_{ std::thread::hardware_concurrency() - 1 },
+    end_of_data_{ false } {
+
+}
+
+template <typename BidirectionalIterator>
+parallel_quick_sorter_t<BidirectionalIterator>::
+~parallel_quick_sorter_t() {
+
+    end_of_data_ = true;
+
+    for (auto && i : threads_) {
+        i.join();
+    }
+}
+
+template <typename BidirectionalIterator>
+void
+parallel_quick_sorter_t<BidirectionalIterator>::
+do_sort(BidirectionalIterator first, BidirectionalIterator last) {
+
+    if (first == last) {
+        return;
+    }
+
+    auto pivot = partition(first, last);
+
+    auto first_chunk = std::make_shared<chunk_to_sort_t>(chunk_to_sort_t{ first, pivot });
+    auto first_task = first_chunk->promise.get_future();
+    chunks_.push(first_chunk);
+
+    if (threads_.size() < max_threads_count_) {
+        threads_.push_back(std::thread{ &parallel_quick_sorter_t<BidirectionalIterator>::sort_thread, this });
+    }
+
+    do_sort(pivot, last);
+
+    while (first_task.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+        sort_thread();
+    }
+}
+
+template <typename BidirectionalIterator>
+void
+parallel_quick_sorter_t<BidirectionalIterator>::
+try_sort_chunk() {
+
+    auto chunk = chunks_.pop();
+    if (chunk)
+    {
+        sort_chunk(*chunk);
+    }
+}
+
+template <typename BidirectionalIterator>
+void
+parallel_quick_sorter_t<BidirectionalIterator>::
+sort_thread() {
+
+    while (!end_of_data_)
+    {
+        try_sort_chunk();
+        std::this_thread::yield();
+    }
+}
+
+template <typename BidirectionalIterator>
+void
+parallel_quick_sorter_t<BidirectionalIterator>::
+sort_chunk(std::shared_ptr<chunk_to_sort_t> chunk) {
+    do_sort(chunk->first, chunk->last);
+    chunk->promise.set_value();
+}
+
+template <typename BidirectionalIterator>
+void
+parallel_quick_sort(BidirectionalIterator first, BidirectionalIterator last) {
+
+    if (first == last) {
+        return;
+    }
+    parallel_quick_sorter_t<BidirectionalIterator> sorter;
+    sorter.do_sort(first, last);
+}
+
+
---
+++ thread_safe_stack.hpp
@@ -0,0 +1,101 @@
+#include <mutex>
+#include <memory>
+#include <iostream>
+
+template <typename T>
+class stack
+{
+
+private:
+	T * array_;
+	bool empty() const;
+	mutable std::mutex mutex_;
+	size_t array_size_;
+	size_t count_;	
+public:
+	stack();
+	size_t count() const;
+	void push(T const &);
+	stack(const stack<T> &);
+	auto pop() -> std::shared_ptr<T>;
+	void swap(stack<T>&);
+};
+
+template <typename T> 
+stack<T>::stack() : count_(0), array_size_(0), array_{ nullptr }
+{}
+
+template <typename T>
+bool stack<T>::empty() const 
+{
+	return (count_ == 0);
+}
+
+template <typename T> 
+stack<T>::stack(const stack<T>& copy)
+{
+	std::lock_guard<std::mutex> lock(copy.mutex_);
+	T *tmp = new T[copy.array_size_];
+	count_ = copy.count_;
+	array_size_ = copy.array_size_;
+	array_ = tmp;
+	try
+	{
+		std::copy(copy.array_, copy.array_ + count_, array_);
+	}
+	catch(...){
+		delete[] array_;
+	}
+}
+
+template <typename T>
+auto stack<T>::pop() -> std::shared_ptr<T>
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (!empty())
+	{
+		--count_;
+		return std::make_shared<T>(array_[count_]);
+	}
+	else
+		throw "stack empty";
+	
+}
+
+template<class T>
+size_t stack<T>::count() const
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	return count_;
+}
+
+template<typename T> 
+void stack<T>::swap(stack& x) 
+{
+	x.mutex_.lock();
+	std::swap(x.array_size_, array_size_);
+	std::swap(count_, x.count_);
+	std::swap(x.array_, array_);
+	x.mutex_.unlock();
+}
+
+template <typename T>
+void stack<T>::push(T const &value)
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (array_size_ == count_) 
+	{
+		array_size_ *= 2;
+		stack<T> temp(*this);
+		swap(temp);
+	}
+	array_[count_] = value;
+	++count_;
+}
+
+
+
+
+
+
+
